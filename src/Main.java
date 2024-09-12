e7f13adc1950b599b49359d8452f8c5fb30836a2
RedSunRnD <vvatutin.rs@gmail.com>
2024-09-13 00:13:56 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 26;
+        double salary = 84_000;
+        double limit = 0;
+        if (age >= 23) {
+            limit = salary * 3;
+            if (salary >= 50_000 && salary < 80_000) {
+                limit = limit * 1.2;
+            } else if (salary >= 80_000) {
+                limit = limit * 1.5;
+            }
+        } else if ( age < 23) {
+            limit = salary * 2;
+            if (salary >= 50_000 && salary <80_000) {
+                limit = limit * 1.2;
+            } else if (salary >= 80_000) {
+                limit = limit * 1.5;
+            }
+        }
+        System.out.println("Ваш лимит на расходы: " + limit + " рублей");
+    }
+}
